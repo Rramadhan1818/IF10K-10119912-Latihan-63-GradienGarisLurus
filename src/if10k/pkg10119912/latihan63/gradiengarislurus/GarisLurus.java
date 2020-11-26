42986ce1a5b95c31519b0428d8f3f9d35644bb9c
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-11-26 21:59:36 +0700

    first add

---
+++ src/if10k/pkg10119912/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */ 
+public interface GarisLurus {
+    public int hitungGradien();
+}
